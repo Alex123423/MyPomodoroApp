4427bd1d8ab961906e698ef81b6a682cac28bb61
Alexey Davidenko <alexeydavidenko@icloud.com>
2022-01-27 13:45:10 +0300

    resolve merge conflict

---
+++ MyPomodoroApp/RestViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RestViewController.swift
+//  MyPomodoroApp
+//
+//  Created by Alexey Davidenko on 20.01.2022.
+//
+
+import UIKit
+
+class RestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
